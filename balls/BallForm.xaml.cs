06f376ebc8fedf2824911fb88e1a89fe776a7478
fksxiz <malyshkin_04@bk.ru>
2023-04-12 17:34:15 +0300

    qweqwe

--- balls/BallForm.xaml.cs
+++ balls/BallForm.xaml.cs
@@ -21,7 +21,7 @@ namespace balls
     /// </summary>
     public partial class BallForm : Window
     {
-        Thread FirstThread = null; 
+        Thread FirstThread = null;
         Thread SecondThread = null;
 
         Color[,] colMat = new Color[MainWindow.BoxWidth, MainWindow.BoxHeigth];
@@ -38,9 +38,9 @@ namespace balls
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
-            for(var row = 0; row< MainWindow.BoxWidth; row++)
+            for (var row = 0; row < MainWindow.BoxWidth; row++)
             {
-                for (var col =0;col< MainWindow.BoxHeigth;col++)
+                for (var col = 0; col < MainWindow.BoxHeigth; col++)
                 {
                     colMat[row, col] = Color.White;
                 }
@@ -55,9 +55,18 @@ namespace balls
 
         }
 
+        private Color GetLayBallColor(int r, int c)
+        {
+            Color res;
+            lock(locker){
+                res = colMat[r, c];
+            }
+            return res;
+        }
+
         private void BallFill(object param)
         {
-            var handle = (IntPtr) param;
+            var handle = (IntPtr)param;
 
             Random random = new Random();
             var gCntx = Graphics.FromHwnd(handle);
@@ -68,6 +77,52 @@ namespace balls
             var fCntx = Graphics.FromImage(frame);
 
             var cleanBrush = new SolidBrush(Color.White);
+
+            while (true)
+            {
+
+                var ballColor = Color.Black;
+                switch (random.Next(colorCount))
+                {
+                    case 0:
+                        ballColor = Color.Azure;
+                        break;
+                    case 1:
+                        ballColor = Color.Red;
+                        break;
+                    case 2:
+                        ballColor = Color.Green;
+                        break;
+                    case 3:
+                        ballColor = Color.Khaki;
+                        break;
+                    default:
+                        ballColor = Color.White;
+                        break;
+                }
+
+                var fallCol = random.Next(MainWindow.BoxWidth);
+                var ballY = 0;
+
+                while (true)
+                {
+                    fCntx.FillRectangle(cleanBrush, 0, 0,
+                    MainWindow.BallSize * MainWindow.BoxWidth,
+                    MainWindow.BallSize * MainWindow.BoxHeigth);
+
+                    var fallRow = ballY / MainWindow.BallSize;
+
+                }
+
+                for (var row = 0; row < MainWindow.BoxWidth; row++)
+                {
+                    for (var col = 0; col < MainWindow.BoxHeigth; col++)
+                    {
+                        var layBallColor = colMat[row, col];
+                    }
+                }
+            }
+
         }
     }
 }
